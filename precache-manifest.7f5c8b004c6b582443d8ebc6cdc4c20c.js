79ac70e342384b146c2137af83f0f69a34f1e4b6
Tetiana <tetianaprivat@gmail.com>
2020-04-09 17:06:08 +0300

    Updates

---
+++ precache-manifest.7f5c8b004c6b582443d8ebc6cdc4c20c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b2f4def4206a12be97a4d1e1b9a698",
+    "url": "/hw3-timer-react-cursor/index.html"
+  },
+  {
+    "revision": "fb3b83b569c1d622093d",
+    "url": "/hw3-timer-react-cursor/static/css/main.a77bd64c.chunk.css"
+  },
+  {
+    "revision": "85cd3d3e782216e46b91",
+    "url": "/hw3-timer-react-cursor/static/js/2.41ceb5d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw3-timer-react-cursor/static/js/2.41ceb5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3b83b569c1d622093d",
+    "url": "/hw3-timer-react-cursor/static/js/main.2525523c.chunk.js"
+  },
+  {
+    "revision": "9bd83a850f2a6ea110db",
+    "url": "/hw3-timer-react-cursor/static/js/runtime-main.caee6ce7.js"
+  }
+]);
